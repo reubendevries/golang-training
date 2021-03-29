fb17a22c18f2608cd56b5c7d2112cf3702a709a5
reubendevries <reubendevries@gmail.com>
2021-03-29 15:11:55 -0700

    finished lesson 12

--- main.go
+++ main.go
@@ -3,25 +3,7 @@ package main
 import "log"
 
 func main() {
-	var whatToSay string
-	var saySomethingElse string
-	var i int
+	names := []string{"one", "seven", "fish", "cat"}
 
-	whatToSay, _ = saySomething("Hello")
-
-	log.Println(whatToSay)
-
-	saySomethingElse, _ = saySomething("Goodbye")
-
-	log.Println(saySomethingElse)
-
-	log.Println(saySomething("Finally"))
-
-	i = 7
-	i = 8
-	log.Println(i)
-}
-
-func saySomething(s string) (string, string) {
-	return s, "world"
+	log.Println(names)
 }
