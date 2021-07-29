b2c47630a1dfbcbcf19c301bd35bddda05cc3c60
reubendevries <reubendevries@gmail.com>
2021-07-28 21:28:24 -0700

    coffee selector

--- main.go
+++ main.go
@@ -1,32 +1,54 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"myapp/doctor"
-	"os"
-	"strings"
+	"log"
+	"strconv"
+
+	"github.com/eiannone/keyboard"
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
+	err := keyboard.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	whatToSay := doctor.Intro()
+	defer func() {
+		_ = keyboard.Close()
+	}()
 
-	fmt.Println(whatToSay)
+	coffees := make(map[int]string)
+	coffees[1] = "Cappucino"
+	coffees[2] = "Latte"
+	coffees[3] = "Americano"
+	coffees[4] = "Mocha"
+	coffees[5] = "Macchiato"
+	coffees[6] = "Espresso"
+
+	fmt.Println("MENU")
+	fmt.Println("----")
+	fmt.Println("1 - Cappucino")
+	fmt.Println("2 - Latte")
+	fmt.Println("3 - Americano")
+	fmt.Println("4 - Mocha")
+	fmt.Println("5 - Macchiato")
+	fmt.Println("6 - Espresso")
+	fmt.Println("Q - Quit the Program")
 
 	for {
-
-		fmt.Print("-> ")
-
-		userInput, _ := reader.ReadString('\n')
-
-		userInput = strings.Replace(userInput, "\n", "", -1)
-
-		if userInput == "quit" {
-			break
-		} else {
-			fmt.Println(doctor.Response(userInput))
+		char, _, err := keyboard.GetSingleKey()
+		if err != nil {
+			log.Fatal(err)
 		}
+
+		if char == 'q' || char == 'Q' {
+			break
+		}
+
+		i, _ := strconv.Atoi(string(char))
+		fmt.Printf("You chose %s \n", coffees[i])
+
 	}
+	fmt.Println("Program Exiting")
 }
