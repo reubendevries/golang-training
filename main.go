2d191ee20746d8fb7c625e08756262d3da6f9d87
reubendevries <reubendevries@gmail.com>
2021-07-30 21:44:10 -0700

    finished working with pointers

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	x := 10
+	myFirstPointer := &x
+	fmt.Println("x is", x)
+	fmt.Println("myFirstPointer is", myFirstPointer)
+	*myFirstPointer = 15
+	fmt.Println("x is now", x)
+	fmt.Println("myFirstPointer is", myFirstPointer)
 }
