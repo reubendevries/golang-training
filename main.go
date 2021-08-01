cf53dacde82c38b8e9bdca6c609e424af2594191
reubendevries <reubendevries@gmail.com>
2021-07-31 20:52:55 -0700

    finished course on maps

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	intMap := make(map[string]int)
+
+	intMap["one"] = 1
+	intMap["two"] = 2
+	intMap["three"] = 3
+	intMap["four"] = 4
+	intMap["five"] = 5
+
+	for key, value := range intMap {
+		fmt.Println(key, value)
+	}
+
+	delete(intMap, "four")
+
+	for key, value := range intMap {
+		fmt.Println(key, value)
+	}
+
+	el, ok := intMap["one"]
+	if ok {
+		fmt.Println(el, "is in the map")
+	} else {
+		fmt.Println(el, "is not in the map")
+	}
 }
