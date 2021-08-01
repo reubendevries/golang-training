f5dabd6ba49d2a45df85436377e343dcc98c8fbe
reubendevries <reubendevries@gmail.com>
2021-07-31 21:44:18 -0700

    keylogger

--- main.go
+++ main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/eiannone/keyboard"
+)
+
+var KeyPressChan chan rune
+
+func main() {
+	KeyPressChan = make(chan rune)
+
+	go listenForKeyPress()
+
+	fmt.Println("Press any key or q to quit.")
+	_ = keyboard.Open()
+	defer func() {
+		keyboard.Close()
+	}()
+
+	for {
+		char, _, _ := keyboard.GetSingleKey()
+		if char == 'q' || char == 'Q' {
+			break
+		}
+
+		KeyPressChan <- char
+	}
+}
+
+func listenForKeyPress() {
+	for {
+		key := <-KeyPressChan
+		fmt.Println("You Pressed", string(key))
+	}
 }
