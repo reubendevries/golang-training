6f034bc5180810e38d3826a6965c6aa90a6371a0
reubendevries <reubendevries@gmail.com>
2021-08-02 20:47:47 -0700

    finished boolean lesson.

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	apples := 18
+	oranges := 9
+
+	// boolean expression
+	fmt.Println(apples == oranges)
+	fmt.Println(apples != oranges)
+
+	// > < >= <=
+
+	fmt.Printf("%d > %d: %t\n", apples, oranges, apples > oranges)
+	fmt.Printf("%d < %d: %t\n", apples, oranges, apples < oranges)
+	fmt.Printf("%d >= %d: %t\n", apples, oranges, apples >= oranges)
+	fmt.Printf("%d <= %d: %t\n", apples, oranges, apples <= oranges)
 }
