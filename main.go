5b825060d8e665b72e713e64c424f2c1c9a7f9c4
reubendevries <reubendevries@gmail.com>
2021-08-02 22:19:57 -0700

    finished exported vs. unexported

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"log"
+	"myapp/staff"
+)
 
+var employees = []staff.Employee{
+	{FirstName: "John", LastName: "Smith", Salary: 30000, FullTime: true},
+	{FirstName: "Sally", LastName: "Jones", Salary: 50000, FullTime: true},
+	{FirstName: "Mark", LastName: "Smithers", Salary: 60000, FullTime: true},
+	{FirstName: "Allen", LastName: "Anderson", Salary: 15000, FullTime: false},
+	{FirstName: "Margaret", LastName: "Carter", Salary: 100000, FullTime: true},
+}
+
+func main() {
+	myStaff := staff.Office{
+		AllStaff: employees,
+	}
+
+	log.Println("Overpaid staff", myStaff.Overpaid())
+	log.Println("Underpaid staff", myStaff.Underpaid())
 }
---
+++ staff/staff.go
@@ -0,0 +1,47 @@
+package staff
+
+import "log"
+
+var OverPaidLimit = 75000
+var underPaidLimit = 20000
+
+type Employee struct {
+	FirstName string
+	LastName  string
+	Salary    int
+	FullTime  bool
+}
+
+type Office struct {
+	AllStaff []Employee
+}
+
+func (e *Office) All() []Employee {
+	return e.AllStaff
+}
+
+func (e *Office) Overpaid() []Employee {
+	var overpaid []Employee
+
+	for _, x := range e.AllStaff {
+		if x.Salary > OverPaidLimit {
+			overpaid = append(overpaid, x)
+		}
+	}
+	return overpaid
+}
+
+func (e *Office) Underpaid() []Employee {
+	var underpaid []Employee
+
+	for _, x := range e.AllStaff {
+		if x.Salary < underPaidLimit {
+			underpaid = append(underpaid, x)
+		}
+	}
+	return underpaid
+}
+
+func (e *Office) notVisiable() {
+	log.Println("Hello, World!")
+}
