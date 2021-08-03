28174542dbbb5db8e336846832f3fdcf36eb7a5f
reubendevries <reubendevries@gmail.com>
2021-08-02 20:31:47 -0700

    finished expressions

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	age := 10
+	name := "Jack"
+	rightHanded := true
+
+	fmt.Printf("%s is %d years old. Right handed: %t\n", name, age, rightHanded)
 }
