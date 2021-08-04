bd41eb764da19c55679640f4e02e4b574ae70f47
reubendevries <reubendevries@gmail.com>
2021-08-03 20:36:06 -0700

    finished infinite loop lesson

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"time"
 
+	"myapp/mylogger"
+)
+
+func main() {
+	// read input from the user 5 times and write it to a log.
+
+	reader := bufio.NewReader(os.Stdin)
+	ch := make(chan string)
+
+	go mylogger.ListenForLog(ch)
+
+	fmt.Println("Enter Something")
+
+	for i := 0; i < 5; i++ {
+		fmt.Print("-> ")
+		input, _ := reader.ReadString('\n')
+		ch <- input
+		time.Sleep(time.Second)
+	}
 }
