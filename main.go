ee0d2f03922777db9687c1cb8568c518203f3c3b
reubendevries <reubendevries@gmail.com>
2021-08-05 21:01:56 -0700

    improved eliza

--- main.go
+++ main.go
@@ -15,18 +15,23 @@ func main() {
 
 	fmt.Println(whatToSay)
 
+	// execute everything in this loop until the user enters 'quit' and presses return.
 	for {
+		// print out a prompt
 		fmt.Print("-> ")
+
+		// wait for user to print something and press enter
 		userInput, _ := reader.ReadString('\n')
 
+		// strip the carriage return off of wahtever they typed.
 		userInput = strings.Replace(userInput, "\r\n", "", -1)
 		userInput = strings.Replace(userInput, "\n", "", -1)
 
-		if userInput == "quit" {
-			break
-		} else {
-			fmt.Println(doctor.Response(userInput))
-		}
+		fmt.Println(doctor.Response(userInput))
 
+		if userInput != "quit" {
+			// end program
+			break
+		}
 	}
 }
