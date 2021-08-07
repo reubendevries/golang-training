7ba4a43ad1bdce2b741e377f254b57c5da1f2c87
reubendevries <reubendevries@gmail.com>
2021-08-06 19:07:02 -0700

    started session-six

--- main.go
+++ main.go
@@ -1,5 +1,65 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"os/exec"
+	"runtime"
+	"strings"
+	"time"
+)
 
+const (
+	ROCK     = 0
+	PAPER    = 1
+	SCISSORS = 2
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	playerChoice := ""
+	playerValue := -1
+	computerValue := rand.Intn(2)
+
+	reader := bufio.NewReader(os.Stdin)
+
+	clearScreen()
+
+	fmt.Print("Please enter rock, paper, or scissors -> ")
+	playerChoice, _ = reader.ReadString('\n')
+	playerChoice = strings.TrimSpace(playerChoice)
+
+	if playerChoice == "rock" {
+		playerValue = ROCK
+	}
+
+	if playerChoice == "paper" {
+		playerValue = PAPER
+	}
+
+	if playerChoice == "scissors" {
+		playerValue = SCISSORS
+	}
+
+	fmt.Println()
+	fmt.Println("player chose", playerChoice, "and value is", playerValue)
+	fmt.Println("Computer chose", computerValue)
+
+}
+
+// clearScreen clears the screen
+func clearScreen() {
+	// windows
+	if strings.Contains(runtime.GOOS, "Windows") {
+		cmd := exec.Command("cmd", "/c", "cls")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	} else {
+		// linux or mac
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
 }
