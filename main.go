3398ffc6a64e73721951fdaed0ec79a84eb3fc00
reubendevries <reubendevries@gmail.com>
2021-08-11 20:10:12 -0700

    finished short circuit

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"fmt"
+)
 
+func main() {
+	a := 12
+	b := 6
+	c, err := divideTwoNumbers(a, b)
+	if err != nil {
+		fmt.Print(err)
+	} else {
+		if c == 2 {
+			fmt.Println("We found 2.")
+		}
+	}
+}
+
+func divideTwoNumbers(x, y int) (int, error) {
+	if y == 0 {
+		return 0, errors.New("cannot divde by zero.\n")
+	}
+	return x / y, nil
 }
