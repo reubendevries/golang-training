9f45f4f8d38b402479655dd61b588139925af576
reubendevries <reubendevries@gmail.com>
2021-08-13 14:15:52 -0700

    finished string-length lesson

--- main.go
+++ main.go
@@ -3,15 +3,24 @@ package main
 import "fmt"
 
 func main() {
+	// indexes start at 0
+	//						 1		   2         3
+	//			   01234567890123456789012345678901234
+
 	courseName := "Learn Go for Beginners Crash Course"
-	fmt.Println(string(courseName[0]))
-	fmt.Println(string(courseName[6]))
 
 	for i := 0; i <= 21; i++ {
 		fmt.Println(string(courseName[i]))
 	}
+	fmt.Println()
 
-	for i := 13; i <= 21; i++ {
-		fmt.Println(string(courseName[i]))
-	}
+	fmt.Println("Length of courseName is: ", len(courseName))
+
+	var mySlice []string
+	mySlice = append(mySlice, "one")
+	mySlice = append(mySlice, "two")
+	mySlice = append(mySlice, "three")
+
+	fmt.Println("mySlice has: ", len(mySlice), "elements")
+	fmt.Println("The last element in mySlice is: ", mySlice[len(mySlice)-1])
 }
