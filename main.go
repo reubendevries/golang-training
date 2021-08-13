9ece7a20fd14f1db623b101b8de1bf57e73f77fa
reubendevries <reubendevries@gmail.com>
2021-08-13 13:59:56 -0700

    finished strings lesson

--- main.go
+++ main.go
@@ -1,5 +1,58 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strings"
+)
 
+func main() {
+	fmt.Println()
+	name := "Hello world"
+	fmt.Println("String: ", name)
+	fmt.Println()
+
+	fmt.Println("Bytes: ")
+	for i := 0; i < len(name); i++ {
+		fmt.Printf("%x ", name[i])
+	}
+	fmt.Println()
+	fmt.Println()
+
+	fmt.Println("Index\tRune\tString")
+	for x, y := range name {
+		fmt.Println(x, "\t", y, "\t", string(y))
+	}
+	fmt.Println()
+	name = "Γειά σου Κόσμε"
+	fmt.Println("Index\tRune\tString")
+	for x, y := range name {
+		fmt.Println(x, "\t", y, "\t", string(y))
+	}
+	fmt.Println()
+
+	fmt.Println()
+	fmt.Println("Three ways to concatenate strings")
+
+	h := "Hello, "
+	w := "world."
+
+	myString := h + w
+
+	fmt.Println(myString)
+
+	// using fmt -- more efficient
+	myString = fmt.Sprintf("%s%s", h, w)
+	fmt.Println(myString)
+	fmt.Println()
+
+	// using stringbuilder - very efficient
+	var sb strings.Builder
+	sb.WriteString(h)
+	sb.WriteString(w)
+	fmt.Println(sb.String())
+
+	fmt.Println()
+	name = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	fmt.Println("Getting a substring")
+	fmt.Println(name[0:25])
 }
