d94d1853c3d26395acf942a1109f881b7a73ddde
reubendevries <reubendevries@gmail.com>
2021-08-13 14:06:28 -0700

    finished indexing

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	courseName := "Learn Go for Beginners Crash Course"
+	fmt.Println(string(courseName[0]))
+	fmt.Println(string(courseName[6]))
+
+	for i := 0; i <= 21; i++ {
+		fmt.Println(string(courseName[i]))
+	}
+
+	for i := 13; i <= 21; i++ {
+		fmt.Println(string(courseName[i]))
+	}
 }
