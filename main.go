714ed88049025de32318d58a45c3b1cb050cecbc
reubendevries <reubendevries@gmail.com>
2021-08-13 20:59:32 -0700

    finished string manipulation

--- main.go
+++ main.go
@@ -6,29 +6,27 @@ import (
 )
 
 func main() {
-	courses := []string{
-		"Learn Go for Beginners Crash Course",
-		"Learn Java for Beginners Crash Course",
-		"Learn Python for Beginners Crash Course",
-		"Learn C for Beginners Crash Course",
-	}
 
-	for _, x := range courses {
-		if strings.Contains(x, "Go") {
-			fmt.Println("Go is found in", x, "and index is", strings.Index(x, "Go"))
-		}
-	}
 	//						1		  2			3		  4
 	//			  0123456789012345678901234567890123456789012345
-	newString := "Go is a great programming language. Go for It!"
+	newString := "Go is a great programming language. Go for it!"
 
-	fmt.Println(strings.HasPrefix(newString, "Go"))
-	fmt.Println(strings.HasPrefix(newString, "Python"))
-	fmt.Println(strings.HasSuffix(newString, "!"))
-	fmt.Println(strings.Count(newString, "Go"))
-	fmt.Println(strings.Count(newString, "Fish"))
-	fmt.Println(strings.Index(newString, "Go"))
-	fmt.Println(strings.Index(newString, "Python"))
-	fmt.Println(strings.LastIndex(newString, "Go"))
-	fmt.Println(strings.LastIndex(newString, "Python"))
+	if strings.Contains(newString, "Go") {
+		newString = strings.ReplaceAll(newString, "Go", "Golang")
+		//newString = strings.Replace(newString, "Go", "Golang", 1)
+	}
+
+	fmt.Println(newString)
+
+	// String Comparision -- works due to run value, a string is just
+	if "Alpha" > "Absolute" {
+		fmt.Println("Alpha is greater than Absolute")
+	} else {
+		fmt.Println("Alpha is not greater than Absolute")
+	}
+
+	badEmail := " me@here.com "
+	badEmail = strings.TrimSpace(badEmail)
+	fmt.Printf("=%s=\n", badEmail)
+	fmt.Println()
 }
