f9153bb6eb838942620fb3ee0fb26ffc9edf9606
reubendevries <reubendevries@gmail.com>
2021-08-14 10:52:25 -0700

    finished hello world web

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello, World!")
+}
+
+func main() {
+	http.HandleFunc("/", homePage)
+	log.Println("Starting WebServer on port 8080")
+	http.ListenAndServe(":8080", nil)
 }
